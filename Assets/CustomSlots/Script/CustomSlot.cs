e1bd0897beb995421843ffa55678b1f0f7f9cfaa
SumeshBharathi <sumeshbharathi@gmail.com>
2025-03-13 12:44:07 -0400

    backup

--- Assets/CustomSlots/Script/CustomSlot.cs
+++ Assets/CustomSlots/Script/CustomSlot.cs
@@ -95,7 +95,7 @@ namespace CSFramework
 			yield return new WaitForSeconds(delayTime);
 
 			//Do the action after the delay time has finished.
-			lineManager.EnableLines(5);
+			lineManager.EnableLines(40);
 
 		}
 
